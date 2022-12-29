bbb2c547ad34d000adac2ef295f8871444c92fb0
Tae Yeong Kim <taeyeong200@gmail.com>
2022-12-29 15:39:48 +1100

    refactor

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const generatemvstoken = require("./generatemvstoken");
 const {Client} = require("multiversus.js");
-const Search = require("./search");
-const Characters = require("./characters");
-require('dotenv').config();
+const Search = require("./lib/search.js");
+const Characters = require("./lib/characters.js");
+const generatemvstoken = require("./lib/generatemvstoken.js");
+const requestdata = require("./lib/requestdata.js");
+const Users = require("./lib/users.js");
+// Create a new Discord client
 (async () => {
     // Get access token and create a new client
     const mvstoken = await generatemvstoken.getAccessToken();
     mvs_client = new Client({ accessToken: mvstoken });
 
-    /*
+    // Get the user ID for the given username
     const user_id = await Search.getidfromusername("keryan666");
     const ranked1s = await Search.getRanked1sData(user_id);
     const ranked2s = await Search.getRanked2sData(user_id);
     console.log("ranked1s:", ranked1s);
     console.log("ranked2s:", ranked2s);
 })().catch((error) => {
-    */
+    console.error(error);
 });
