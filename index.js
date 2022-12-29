cd33aef668b9bf4a4daaa9005c10b67ad7d20530
Tae Yeong Kim <taeyeong200@gmail.com>
2022-12-29 22:36:24 +1100

    fix

--- index.js
+++ index.js
@@ -5,15 +5,6 @@ const generatemvstoken = require("./lib/generatemvstoken.js");
 const requestdata = require("./lib/requestdata.js");
 const Users = require("./lib/users.js");
 const Maps = require("./lib/maps.js");
-// Create a new Discord client
-(async () => {
-    // Get access token and create a new client
-    const mvstoken = await generatemvstoken.getAccessToken;
-    mvs_client = new Client({ accessToken: mvstoken });
-
-})().catch((error) => {
-    console.error(error);
-});
 
 module.exports.Search = Search;
 module.exports.Characters = Characters;
